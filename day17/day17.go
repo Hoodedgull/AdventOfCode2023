d6fe1497591e90222314025f61db81ae5c5d9041
Victor <victor.s@live.dk>
2023-12-17 19:47:35 +0100

    day 17 part 2

--- day17/day17.go
+++ day17/day17.go
@@ -62,7 +62,9 @@ func main() {
 				fmt.Println("Found one way with cost: ", opt.cost)
 				if opt.cost < currentBestCost {
 					currentBestCost = opt.cost
-					bestBeam = beam
+					bestBeam = opt
+					newBeams = []Beam{}
+					break
 				}
 			}
 			currentBestCost := costGrid[opt.y][opt.x]
@@ -75,27 +77,26 @@ func main() {
 				bestCostForDir = make(map[int]int)
 				currentBestCost[opt.dir] = bestCostForDir
 			}
-			s := []rune(opt.path)
-			last3 := string(s[max(len(s)-3, 0):])
 
-			stepSize := 0
-			if strings.HasSuffix(last3, strings.Repeat(opt.dir, 3)) {
-				stepSize = 3
-			} else if strings.HasSuffix(last3, strings.Repeat(opt.dir, 2)) {
-				stepSize = 2
-			} else if strings.HasSuffix(last3, strings.Repeat(opt.dir, 1)) {
-				stepSize = 1
-			}
+			stepSize := countSuffix(beam, opt.dir)
 			foundAGoodCase := false
-			for step := stepSize; step <= 3; step++ {
+			foundABetterCase := false
+			for step := 0; step <= 10; step++ {
 				bestCostForStepSize := bestCostForDir[step]
 				if bestCostForStepSize == 0 || opt.cost < bestCostForStepSize {
-					bestCostForDir[stepSize] = opt.cost
-					foundAGoodCase = true
+					if step >= stepSize {
+						bestCostForDir[stepSize] = opt.cost
+					}
+					if step == stepSize {
+						foundAGoodCase = true
+					}
+					if step < stepSize && bestCostForStepSize != 0 && bestCostForStepSize < opt.cost {
+						foundABetterCase = true
+					}
 				}
 			}
 
-			if foundAGoodCase {
+			if foundAGoodCase && !foundABetterCase {
 				//costGrid[opt.y][opt.x] = opt.cost
 				newBeams = append(newBeams, opt)
 			}
@@ -114,7 +115,7 @@ func main() {
 
 	fmt.Println("Got to an exit?!")
 	fmt.Println(currentBestCost)
-	fmt.Println(bestBeam)
+	fmt.Println(bestBeam) // Found 947-> too high
 
 	// fmt.Println("Best cost", costGrid[goaly][goalx])
 
@@ -124,45 +125,75 @@ func main() {
 
 }
 
+func countSuffix(beam Beam, dir string) int {
+	s := []rune(beam.path)
+	last3 := string(s[max(len(s)-10, 0):])
+
+	stepSize := 0
+	for step := 10; step > 0; step-- {
+		if strings.HasSuffix(last3, strings.Repeat(dir, step)) {
+			stepSize = step
+			break
+		}
+	}
+	return stepSize
+}
+
 func getOptions(beam Beam, grid [][]int) []Beam {
 	beams := []Beam{}
-	if beam.dir != "w" && !strings.HasSuffix(beam.path, "eee") {
+	esInEnd := countSuffix(beam, "e")
+	wsInEnd := countSuffix(beam, "w")
+	nsInEnd := countSuffix(beam, "n")
+	ssInEnd := countSuffix(beam, "s")
+	isForcedNorth := nsInEnd > 0 && nsInEnd < 4
+	isForcedEast := esInEnd > 0 && esInEnd < 4
+	isForcedSouth := ssInEnd > 0 && ssInEnd < 4
+	isForcedWest := wsInEnd > 0 && wsInEnd < 4
+	if beam.dir != "w" && esInEnd < 10 && !isForcedNorth && !isForcedSouth && !isForcedWest {
 		x := beam.x + 1
 		y := beam.y
 
-		if isWithinGrid(x, y, grid) {
+		if isWithinGrid(x, y, grid) && canStopInGoal(x, y, grid, esInEnd+1, 0) {
 			newCost := grid[y][x]
 			beams = append(beams, Beam{x: x, y: y, dir: "e", cost: beam.cost + newCost, path: beam.path + "e"})
 		}
 	}
-	if beam.dir != "e" && !strings.HasSuffix(beam.path, "www") {
+	if beam.dir != "e" && wsInEnd < 10 && !isForcedNorth && !isForcedSouth && !isForcedEast {
 		x := beam.x - 1
 		y := beam.y
 
-		if isWithinGrid(x, y, grid) {
+		if isWithinGrid(x, y, grid) && canStopInGoal(x, y, grid, esInEnd, ssInEnd) {
 			newCost := grid[y][x]
 			beams = append(beams, Beam{x: x, y: y, dir: "w", cost: beam.cost + newCost, path: beam.path + "w"})
 		}
 	}
-	if beam.dir != "s" && !strings.HasSuffix(beam.path, "nnn") {
+	if beam.dir != "s" && nsInEnd < 10 && !isForcedEast && !isForcedSouth && !isForcedWest {
 		x := beam.x
 		y := beam.y - 1
 
-		if isWithinGrid(x, y, grid) {
+		if isWithinGrid(x, y, grid) && canStopInGoal(x, y, grid, 0, ssInEnd+1) {
 			newCost := grid[y][x]
 			beams = append(beams, Beam{x: x, y: y, dir: "n", cost: beam.cost + newCost, path: beam.path + "n"})
 		}
 	}
-	if beam.dir != "n" && !strings.HasSuffix(beam.path, "sss") {
+	if beam.dir != "n" && ssInEnd < 10 && !isForcedNorth && !isForcedEast && !isForcedWest {
 		x := beam.x
 		y := beam.y + 1
 
-		if isWithinGrid(x, y, grid) {
+		if isWithinGrid(x, y, grid) && canStopInGoal(x, y, grid, esInEnd, ssInEnd) {
 			newCost := grid[y][x]
 			beams = append(beams, Beam{x: x, y: y, dir: "s", cost: beam.cost + newCost, path: beam.path + "s"})
 		}
 	}
 	return beams
+}
+
+func canStopInGoal(x int, y int, grid [][]int, esInEnd int, ssInEnd int) bool {
+	if y < len(grid)-1 || x < len(grid[0])-1 {
+		return true
+	}
+
+	return esInEnd >= 4 || ssInEnd >= 4
 }
 
 func isWithinGrid(x int, y int, grid [][]int) bool {
